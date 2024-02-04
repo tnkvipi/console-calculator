81428bb68f9ec9733fc0ccdc8c877aeff4925801
eewpo <eewpo@DESKTOP-6Q0QDSG>
2024-02-04 11:59:05 +0300

    Добавьте файлы проекта.

---
+++ console calculator/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+
+namespace console_calculator
+{
+    internal class Program
+    {
+
+        static void Main()
+        {
+            while (true)
+            {
+                Console.Clear();
+                double a, b;
+                string c;
+                try
+                {
+                    Console.WriteLine("enter a number ");
+                    a = double.Parse(Console.ReadLine());
+                    Console.WriteLine("enter a number ");
+                    b = double.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("The wrong symbol!!");
+                    Console.ReadLine();
+                    continue;
+                }
+                Console.WriteLine("select an operation: '+' '-' '*' '/' ");
+                c = Console.ReadLine();
+                switch (c)
+                {
+                    case "+":
+                        Console.WriteLine(a + b);
+                        break;
+                    case "-":
+                        Console.WriteLine(a - b);
+                        break;
+                    case "*":
+                        Console.WriteLine(a * b);
+                        break;
+                    case "/":
+                        Console.WriteLine(a / b);
+                        break;
+                    default:
+                        Console.WriteLine("Mistake! Try again!");
+                        break;
+                }
+
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
